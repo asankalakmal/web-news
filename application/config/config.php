9cc354bcef04ff6b66d3ff093ddf2fd65c565d54
Asanka Lakmal <asankalakmal@gmail.com>
2015-10-14 17:27:14 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://192.168.0.100/portal';
+$config['base_url'] = 'http://192.168.0.100/web-news';
 
 /*
 |--------------------------------------------------------------------------
